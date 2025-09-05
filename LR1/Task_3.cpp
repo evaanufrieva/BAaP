f31e19d6bd52342cffb1e3d18f1c29da5821feec
evaanufrieva <evaupwork25@gmail.com>
2025-09-05 22:58:05 +0300

    добавила библиотеки, void заменила на init

--- LR1/Task_3.cpp
+++ LR1/Task_3.cpp
@@ -1,4 +1,6 @@
-void main() { 
+#include <iostream>
+#include <cmath>
+int main() { 
     double touble, start, end, step; 
     std::cout << "Введите начальное значение x: "; 
     std::cin >> start; // Задаем исходное значение переменной 
@@ -6,7 +8,7 @@ void main() {
     std::cin >> end; // Задаем конечно значение переменной 
     std::cout << "Введите шаг: "; 
     std::cin >> step; // Задаем шаг, с которым с которым будет изменяться 
-    аргумент 
+    //аргумент 
     for(double x = start; x <= end; x += step) { 
         double y = sin(x); // функция y = sin(x)   
         std::cout << x << " | " << y << std::endl; // Выводим значения 
