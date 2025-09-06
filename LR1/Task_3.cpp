ea7b00fef3eca64d4caa748626225c12b986ff4a
evaanufrieva <evaupwork25@gmail.com>
2025-09-06 12:33:31 +0300

    финальный коммит

--- LR1/Task_3.cpp
+++ LR1/Task_3.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 #include <cmath>
-int main() { 
-    double touble, start, end, step; 
-    std::cout << "Введите начальное значение x: "; 
-    std::cin >> start; // Задаем исходное значение переменной 
+int main()
+{
+    double start, end, step;
+    std::cout << "Введите  начальное значение x: ";
+    std::cin >> start; // Задаем исходное значение переменной
     std::cout << "Введите конечное значение x: ";
-    std::cin >> end; // Задаем конечно значение переменной 
-    std::cout << "Введите шаг: "; 
-    std::cin >> step; // Задаем шаг, с которым с которым будет изменяться 
-    //аргумент 
-    for(double x = start; x <= end; x += step) { 
-        double y = sin(x); // функция y = sin(x)   
-        std::cout << x << " | " << y << std::endl; // Выводим значения 
-        функции для каждого аргумента (x | y) 
-    } 
-    return nullptr; 
+    std::cin >> end; // Задаем конечное значение переменной
+    std::cout << "Введите шаг: "; // Задаем шаг, с которым с которым будет изменяться аргумент
+    std::cin >> step;
+    for (double x = start; x <= end; x += step)
+    {
+        double y = sin(x); // функция y = sin(x)
+        std::cout << x << " | " << y << std::endl; // Выводим значения функции для каждого аргумента(x | y)
+    }
+    return 0;
 }
