133483eb7aaf6b0fa08af3ce2167dcdbc64a5b89
evaanufrieva <evaupwork25@gmail.com>
2025-09-16 16:12:57 +0300

    LR2 Tasks 5 main branch

---
+++ LR2/Task_5/Task_5.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(){
+    float a,b;
+    std::cout<<"Enter a:";
+    std::cin>>a;
+    std::cout<<"Enter b:";
+    std::cin>>b;
+    float max=(a>b) ? a:b;
+    std::cout<<"Bigger num is "<<max<<"\n";
+}
