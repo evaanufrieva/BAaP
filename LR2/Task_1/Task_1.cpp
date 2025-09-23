39572b7ae9c9f1b1fe432f25e4169ccadd79408e
evaanufrieva <evaupwork25@gmail.com>
2025-09-23 13:48:26 +0300

    m

--- LR2/Task_1/Task_1.cpp
+++ LR2/Task_1/Task_1.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
+using namespace std;
+int main() {
+    double x;
+    cout << "x: ";
+    cin >> x;
 
-int main(){
-    int x;
-    std::cout << "x: ";
-    std::cin >> x;
-    int x2 = x * x;
-    int v1 = x * (23 * x + 32);
-    int v2 = 69 * x2 + 8;
-    std::cout << v1 + v2 << std::endl << v2 - v1 << std::endl;
+    double x2 = x * x;     
+    double v1 = x * (23 * x + 32);   
+    double v2 = 69 * x2 + 8;  
+
+    double f1 = v1 * x + v2;        
+    double f2 = v2 - v1 * x;
+
+    cout << f1 << endl << f2 << endl;
     return 0;
 }
